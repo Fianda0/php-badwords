aeeb6ca1c4d24e039d02c57bac4db8609707f096
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-06-10 15:23:06 +0200

    Creazione form

--- form.php
+++ form.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+    <form action="stampa.php" method='Get   '>
+
+        <h1>Inserire un testo per sapere la lunghezza</h1>
+
+        <input type="text" placeholder="Inserire il testo" name="testo">
+        <button type="submit">Invia</button>
+
+    </form>
+
 </body>
+
 </html>
